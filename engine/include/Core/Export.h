43b4dac6eddafcaaff14dee5921d1324d26caf0e
xMathfreak <mathewodueyingbo@gmail.com>
2025-11-30 08:35:24 +0100

    feat: Add Linux/macOS support for shared library symbol visibility

--- engine/include/Core/Export.h
+++ engine/include/Core/Export.h
@@ -6,10 +6,12 @@
 	#else
 		#define BLACKTHORN_API __declspec(dllimport)
 	#endif
-#else
+#elif defined(__linux__) || defined(__unix__) || defined(__APPLE__)
 	#ifdef BLACKTHORN_EXPORTS
 		#define BLACKTHORN_API __attribute__((visibility("default")))
 	#else
 		#define BLACKTHORN_API
 	#endif
+#else
+	#define BLACKBLACKTHORN_API
 #endif
