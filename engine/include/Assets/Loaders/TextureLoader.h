f77c7046ce549bc29059777bd7f09c37874b46f2
xMathfreak <mathewodueyingbo@gmail.com>
2026-01-05 01:30:20 +0100

    refactor(assets): Update texture loader to use descriptors

--- engine/include/Assets/Loaders/TextureLoader.h
+++ engine/include/Assets/Loaders/TextureLoader.h
@@ -7,7 +7,8 @@ namespace Blackthorn::Graphics {
 
 class TextureLoader : public Assets::IAssetLoader<Texture> {
 public:
-	std::unique_ptr<Graphics::Texture> load(const std::string& path) override {
+	std::unique_ptr<Graphics::Texture> load(const Assets::LoadParams& params) override {
+		const auto& path = static_cast<const Assets::PathLoadParams&>(params).path;
 		return std::make_unique<Texture>(path);
 	}
 
