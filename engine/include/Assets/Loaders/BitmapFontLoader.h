0642abcac7e005001accf03beff517e155dcc2ca
xMathfreak <mathewodueyingbo@gmail.com>
2026-01-05 01:31:25 +0100

    feat(assets): Add bitmap font loader

---
+++ engine/include/Assets/Loaders/BitmapFontLoader.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Assets/IAssetLoader.h"
+#include "Fonts/BitmapFont.h"
+#include "Graphics/Renderer.h"
+
+namespace Blackthorn {
+
+struct BLACKTHORN_API BitmapParams : Assets::LoadParams {
+	std::string texturePath;
+	std::string metricsPath;
+
+	BitmapParams(const std::string& texture, const std::string& metrics)
+		: texturePath(texture)
+		, metricsPath(metrics)
+	{}
+
+	std::unique_ptr<LoadParams> clone() const override {
+		return std::make_unique<BitmapParams>(*this);
+	}
+};
+
+class BitmapFontLoader : public Assets::IAssetLoader<BitmapFont> {
+private:
+	Graphics::Renderer* renderer;
+
+public:
+	BitmapFontLoader(Graphics::Renderer* ren)
+		: renderer(ren)
+	{}
+
+	std::unique_ptr<BitmapFont> load(const Assets::LoadParams& params) override {
+		const auto& p = static_cast<const BitmapParams&>(params);
+		std::unique_ptr<BitmapFont> font = std::make_unique<BitmapFont>(renderer);
+		font->loadFromFile(p.texturePath, p.metricsPath);
+		return font;
+	}
+
+	std::vector<std::string> getSupportedExtensions() const override {
+		return { ".bmf", ".fnt" };
+	}
+};
+
+} // namespace Blackthorn
