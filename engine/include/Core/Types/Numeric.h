e1072a10360ad1add566701f24149277474da288
xMathfreak <mathewodueyingbo@gmail.com>
2026-09-17 21:17:22 +0100

    refactor(cpp): replace C macro with C++ std::numeric_limits

--- engine/include/Core/Types/Numeric.h
+++ engine/include/Core/Types/Numeric.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <limits>
 
 using U8  = uint8_t;
 using I8  = int8_t;
@@ -20,22 +21,22 @@ using IMAX = intmax_t;
 using F32 = float;
 using F64 = double;
 
-constexpr U8  U8_MAX  = UINT8_MAX;
-constexpr I8  I8_MAX  = INT8_MAX;
-constexpr I8  I8_MIN  = INT8_MIN;
+constexpr U8  U8_MAX  = std::numeric_limits<U8>::max();
+constexpr I8  I8_MAX  = std::numeric_limits<I8>::max();
+constexpr I8  I8_MIN  = std::numeric_limits<I8>::min();
 
-constexpr U16 U16_MAX = UINT16_MAX;
-constexpr I16 I16_MAX = INT16_MAX;
-constexpr I16 I16_MIN = INT16_MIN;
+constexpr U16 U16_MAX = std::numeric_limits<U16>::max();
+constexpr I16 I16_MAX = std::numeric_limits<I16>::max();
+constexpr I16 I16_MIN = std::numeric_limits<I16>::min();
 
-constexpr U32 U32_MAX = UINT32_MAX;
-constexpr I32 I32_MAX = INT32_MAX;
-constexpr I32 I32_MIN = INT32_MIN;
+constexpr U32 U32_MAX = std::numeric_limits<U32>::max();
+constexpr I32 I32_MAX = std::numeric_limits<I32>::max();
+constexpr I32 I32_MIN = std::numeric_limits<I32>::min();
 
-constexpr U64 U64_MAX = UINT64_MAX;
-constexpr I64 I64_MAX = INT64_MAX;
-constexpr I64 I64_MIN = INT64_MIN;
+constexpr U64 U64_MAX = std::numeric_limits<U64>::max();
+constexpr I64 I64_MAX = std::numeric_limits<I64>::max();
+constexpr I64 I64_MIN = std::numeric_limits<I64>::min();
 
-constexpr UMAX UMAX_MAX = UINTMAX_MAX;
-constexpr IMAX IMAX_MAX = INTMAX_MAX;
-constexpr IMAX IMAX_MIN = INTMAX_MIN;
+constexpr UMAX UMAX_MAX = std::numeric_limits<UMAX>::max();
+constexpr IMAX IMAX_MAX = std::numeric_limits<IMAX>::max();
+constexpr IMAX IMAX_MIN = std::numeric_limits<IMAX>::min();
